3c1d92a540d9d89f0d48aea4c70ee65c4379574e
HelbertCLinUPT <102819429+HelbertCLinUPT@users.noreply.github.com>
2024-12-07 16:45:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,78 +4,20 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-
+    mongodbatlas = {
+      source  = "mongodb/mongodbatlas"
+      version = "~> 1.10.0"
+    }
+    grafana = {
+      source  = "grafana/grafana"
+      version = "~> 1.28.0"
+    }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
 }
-
-#provider "mongodbatlas" {
-#  public_key  = var.mongodb_atlas_public_key
-#  private_key = var.mongodb_atlas_private_key
-#}
-
-# provider "grafana" {
-#   cloud_api_key = var.grafana_cloud_api_key 
-# }
-
-# Proveedor para recursos dentro del stack de Grafana
-#provider "grafana" {
-#  alias = "stack"
-#  url   = var.grafana_url
-#  auth  = var.grafana_auth
-#}
-
-
-# module "mongodb" {
-#   source = "./modules/mongodb"
-
-#   providers = {
-#     mongodbatlas = mongodbatlas
-#   }
-
-#   project_name   = var.mongodb_project_name
-#   atlas_org_id   = var.mongodb_atlas_org_id
-#   cluster_name   = var.mongodb_cluster_name
-#   region         = var.mongodb_region
-#   db_username    = var.mongodb_username
-#   db_password    = var.mongodb_password
-#   database_name  = var.mongodb_database_name
-# }
-
-
-
-#module "grafana" {
-#  source = "./modules/grafana"
-  
-#  providers = {
-#    grafana = grafana.stack
-#  }
-
-#  mongodb_username      = var.mongodb_username
-#  mongodb_password      = var.mongodb_password
-#  mongodb_cluster_name  = var.mongodb_cluster_name
-#  mongodb_database_name = var.mongodb_database_name
-#  mongodb_conection_1   = "mongodb+srv://admin:securepassword@my-cluster.t3laz.mongodb.net/?retryWrites=true&w=majority&appName=my-cluster"
-#}
-
-
-# module "web_app" {
-#   source = "./modules/web_app"
-  
-#   app_bucket_name = var.react_app_bucket_name
-#   environment     = var.environment
-# }
-
-
-# module "mobile_app" {
-#   source = "./modules/mobile_app"
-  
-#   app_bucket_name = var.flutter_app_bucket_name
-#   environment     = var.environment
-# }
 
 
 
